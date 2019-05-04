1ec79931066253d821e2cd96c98eb18093d4407a
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-05-04 10:56:54 +0200

    push it, push it real good 💁‍♀️

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # generate-pdf
 generate pdf from html
+
+
+
+https://developer.mozilla.org/en-US/docs/Web/CSS/filter
+
+https://getbootstrap.com/
+
+https://www.pdftron.com/blog/react/react-to-pdf/
+
+https://expressjs.com/en/starter/static-files.html
+
+https://www.pexels.com
---
+++ index.js
@@ -0,0 +1,53 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+const path = require('path');
+
+const app = express();
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(express.static('public'));
+
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/pdf', (req, res) => {
+    console.log(req.body.site);
+
+    const { site } = req.body;
+
+    res.writeHead(200, {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'attachment;filename="website.pdf"'
+    });
+    createPdf(site).then(pdf => {
+        res.end(pdf);
+    });
+});
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+);
+
+const createPdf = async site => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const options = {
+        //path: 'pdf/web.pdf',
+        format: 'A4',
+        printBackground: true
+    };
+
+    await page.goto(site, {
+        waitUntil: 'networkidle2'
+    });
+
+    const pdf = await page.pdf(options);
+    await browser.close();
+    return pdf;
+};
+
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
