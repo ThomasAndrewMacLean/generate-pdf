fb2e4fba8d934296621ebeffa76fe403dd1d8889
Thomas MacLean <thomas.maclean@gmail.com>
2019-05-20 14:00:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
+
+
+var cors = require('cors')
+
 const path = require('path');
 
 const app = express();
+app.use(cors())
+
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use(express.static('public'));
 
